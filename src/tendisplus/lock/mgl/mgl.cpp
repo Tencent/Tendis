f2e75ebcaaa1dbec234e8438ef66fccd62a8ce78
deyukong <deyukong@tencent.com>
2018-09-10 19:20:00 +0800

    add intention lock support

---
+++ src/tendisplus/lock/mgl/mgl.cpp
@@ -0,0 +1,87 @@
+#include "tendisplus/utils/invariant.h"
+#include "tendisplus/lock/mgl/mgl.h"
+#include "tendisplus/lock/mgl/mgl_mgr.h"
+
+namespace tendisplus {
+namespace mgl {
+
+std::atomic<uint64_t> MGLock::_idGen(0);
+std::list<MGLock*> MGLock::_dummyList{};
+
+MGLock::MGLock()
+    :_id(_idGen.fetch_add(1, std::memory_order_relaxed)),
+     _target(""),
+     _targetHash(0),
+     _mode(LockMode::LOCK_NONE),
+     _res(LockRes::LOCKRES_UNINITED),
+     _resIter(_dummyList.end()) {
+}
+
+MGLock::~MGLock() {
+    INVARIANT(_res == LockRes::LOCKRES_UNINITED);
+}
+
+void MGLock::releaseLockResult() {
+    std::lock_guard<std::mutex> lk(_mutex);
+    _res = LockRes::LOCKRES_UNINITED;
+    _resIter = _dummyList.end();
+}
+
+void MGLock::setLockResult(LockRes res, std::list<MGLock*>::iterator iter) {
+    std::lock_guard<std::mutex> lk(_mutex);
+    _res = res;
+    _resIter = iter;
+}
+
+void MGLock::unlock() {
+    LockRes status = getStatus();
+    INVARIANT(status == LockRes::LOCKRES_OK || status == LockRes::LOCKRES_WAIT);
+    MGLockMgr::getInstance().unlock(this);
+    status = getStatus();
+    INVARIANT(status == LockRes::LOCKRES_UNINITED);
+}
+
+LockRes MGLock::lock(const std::string& target, LockMode mode,
+                     uint64_t timeoutMs) {
+    _target = target;
+    _mode = mode;
+    INVARIANT(getStatus() == LockRes::LOCKRES_UNINITED);
+    _resIter = _dummyList.end();
+    if (_target != "") {
+        _targetHash = static_cast<uint64_t>(std::hash<std::string>{}(_target));
+    } else {
+        _targetHash = 0;
+    }
+    MGLockMgr::getInstance().lock(this);
+    if (getStatus() == LockRes::LOCKRES_OK) {
+        return LockRes::LOCKRES_OK;
+    }
+    if (waitLock(timeoutMs)) {
+        return LockRes::LOCKRES_OK;
+    } else {
+        return LockRes::LOCKRES_TIMEOUT;
+    }
+}
+
+std::list<MGLock*>::iterator MGLock::getLockIter() const {
+    return _resIter;
+}
+
+void MGLock::notify() {
+    _cv.notify_one();
+}
+
+bool MGLock::waitLock(uint64_t timeoutMs) {
+    std::unique_lock<std::mutex> lk(_mutex);
+    return _cv.wait_for(lk,
+                        std::chrono::milliseconds(timeoutMs),
+                        [this]() { return _res == LockRes::LOCKRES_OK; });
+}
+
+LockRes MGLock::getStatus() const {
+    std::lock_guard<std::mutex> lk(_mutex);
+    return _res;
+}
+
+}  // namespace mgl
+}  // namespace tendisplus
---
+++ src/tendisplus/lock/mgl/mgl_mgr.h
@@ -0,0 +1,62 @@
+#ifndef SRC_TENDISPLUS_LOCK_MGL_MGL_MGR_H__
+#define SRC_TENDISPLUS_LOCK_MGL_MGL_MGR_H__
+
+#include <vector>
+#include <list>
+#include <unordered_map>
+#include <mutex>
+#include <string>
+
+#include "tendisplus/lock/mgl/lock_defines.h"
+
+namespace tendisplus {
+namespace mgl {
+
+class MGLock;
+
+// TODO(deyukong): this class should only be in mgl_mgr.cpp
+// not thread safe, protected by LockShard's mutex
+class LockSchedCtx {
+ public:
+    LockSchedCtx();
+    LockSchedCtx(LockSchedCtx&&) = default;
+    void lock(MGLock* core);
+    void unlock(MGLock* core);
+ private:
+    void schedPendingLocks();
+    void incrPendingRef(LockMode mode);
+    void incrRunningRef(LockMode mode);
+    void decPendingRef(LockMode mode);
+    void decRunningRef(LockMode mode);
+    uint16_t _runningModes;
+    uint16_t _pendingModes;
+    std::vector<uint16_t> _runningRefCnt;
+    std::vector<uint16_t> _pendingRefCnt;
+    std::list<MGLock*> _runningList;
+    std::list<MGLock*> _pendingList;
+};
+
+// class alignas(std::hardware_destructive_interference_size) LockShard {
+// hardware_destructive_interference_size requires quite high version
+// gcc. 128 should work for most cases
+
+struct alignas(128) LockShard {
+    std::mutex mutex;
+    std::unordered_map<std::string, LockSchedCtx> map;
+};
+
+class MGLockMgr {
+ public:
+    void lock(MGLock* core);
+    void unlock(MGLock* core);
+    static MGLockMgr& getInstance();
+ private:
+    MGLockMgr() = default;
+    static constexpr size_t SHARD_NUM = 32;
+    LockShard _shards[SHARD_NUM];
+};
+
+}  // namespace mgl
+
+}  // namespace tendisplus
+#endif  // SRC_TENDISPLUS_LOCK_MGL_MGL_MGR_H__
