fe6c7070242ea9007f14d24ad228c9204f812c13
deyukong <deyukong@tencent.com>
2018-09-16 21:26:03 +0800

    put get/set into kv

--- src/tendisplus/commands/kv.cpp
+++ src/tendisplus/commands/kv.cpp
@@ -169,4 +169,47 @@ class SetCommand: public Command {
     }
 } setCommand;
 
+class GetCommand: public Command {
+ public:
+    GetCommand()
+        :Command("get") {
+    }
+
+    ssize_t arity() const {
+        return 2;
+    }
+
+    int32_t firstkey() const {
+        return 1;
+    }
+
+    int32_t lastkey() const {
+        return 1;
+    }
+
+    int32_t keystep() const {
+        return 1;
+    }
+
+    Expected<std::string> run(NetSession *sess) final {
+        SessionCtx *pCtx = sess->getCtx();
+        INVARIANT(pCtx != nullptr);
+        const std::string& key = sess->getArgs()[1];
+        RecordKey rk(pCtx->getDbId(), RecordType::RT_KV, key, "");
+        uint32_t storeId = Command::getStoreId(sess, key);
+        Expected<RecordValue> rv =
+            Command::expireKeyIfNeeded(sess, storeId, rk);
+        if (rv.status().code() == ErrorCodes::ERR_EXPIRED) {
+            return fmtNull();
+        } else if (rv.status().code() == ErrorCodes::ERR_NOTFOUND) {
+            return fmtNull();
+        } else if (!rv.status().ok()) {
+            return rv.status();
+        } else {
+            return fmtBulk(rv.value().getValue());
+        }
+    }
+} getCommand;
+
+
 }  // namespace tendisplus
