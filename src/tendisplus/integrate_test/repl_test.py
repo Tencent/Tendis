932cac03fdb111e482f65e05e454ce2967101dd8
deyukong <deyukong@tencent.com>
2018-09-25 15:54:38 +0800

    refine integrate test

--- src/tendisplus/integrate_test/repl_test.py
+++ src/tendisplus/integrate_test/repl_test.py
@@ -2,29 +2,105 @@
 
 import time
 import redis
+import threading
 from startstop import RedisServer
 
-def test_repl1(m, s):
-    time.sleep(3)
-    cli1 = redis.Connection(host="127.0.0.1", port=12345)
-    cli2 = redis.Connection(host="127.0.0.1", port=12346)
 
-    cli1.send_command("set", "a", "1")
-    assert(cli1.read_response() == "OK")
-    cli1.send_command("set", "b", "2")
-    assert(cli1.read_response() == "OK")
+def test_repl_match2(m, s):
+    running = True 
+    lk = threading.Lock()
+    lst = []
+    thd_lst = []
+    def insert(svr, idx):
+        print "thread:", idx, "starts"
+        cli = redis.Connection(host="127.0.0.1", port=svr.port)
+        # assume base on 10000000000, never overlaps
+        cnt = idx*10000000000
+        while running:
+            cnt += 1
+            lk.acquire()
+            lst.append(cnt)
+            lk.release()
+            cli.send_command("set", str(cnt), str(cnt))
+            res = cli.read_response()
+            assert(res == "OK")
+        print "thread:", idx, "stops"
 
-    cli2.send_command("slaveof", "127.0.0.1", "12345")
-    print cli2.read_response()
-    time.sleep(1)
-    cli2.send_command("get", "a")
-    assert(cli2.read_response() == "1")
-    cli2.send_command("get", "b")
-    assert(cli2.read_response() == "2")
+    for i in xrange(10):
+        a = threading.Thread(target=insert, args=(m, i))
+        a.start()
+        thd_lst.append(a)
+
+    time.sleep(10)
+    lk.acquire()
+    print "list size:", len(lst)
+    lk.release()
+
+    cli1 = redis.Connection(host="127.0.0.1", port=m.port)
+    cli2 = redis.Connection(host="127.0.0.1", port=s.port)
+    cli2.send_command("slaveof", "127.0.0.1", str(m.port))
+    res = cli2.read_response()
+    assert(res == "OK")
+
+    time.sleep(10)
+    running = False
+    for o in thd_lst:
+        o.join()
+
+    cli1.send_command("binlogpos", "0")
+    m_pos = int(cli1.read_response())
+    sleep_cnt = 0
+
+    while True:
+        cli2.send_command("binlogpos", "0")
+        s_pos = int(cli2.read_response())
+        assert(s_pos <= m_pos)
+        if (s_pos == m_pos):
+            print "m/s binlog matches"
+            break
+        else:
+            print "m/s binlog not matches", m_pos, s_pos
+            if (sleep_cnt >= 30):
+                assert 0, "m/s binlogpos not match"
+            time.sleep(1)
+            sleep_cnt += 1
+
+    for o in lst:
+        cli1.send_command("get", str(i))
+        assert(cli1.read_response() == str(i))
+        cli2.send_command("get", str(i))
+        assert(cli2.read_response() == str(i))
+    print "test_repl_match2 pass"
+
+def test_repl_match1(m, s):
+    cli1 = redis.Connection(host="127.0.0.1", port=m.port)
+    cli2 = redis.Connection(host="127.0.0.1", port=s.port)
+
+    cli2.send_command("slaveof", "127.0.0.1", str(m.port))
+    res = cli2.read_response()
+    assert(res == "OK")
 
     for i in xrange(10000):
         cli1.send_command("set", str(i), str(i))
         cli1.read_response()
+
+    cli1.send_command("binlogpos", "0")
+    m_pos = int(cli1.read_response())
+    sleep_cnt = 0
+
+    while True:
+        cli2.send_command("binlogpos", "0")
+        s_pos = int(cli2.read_response())
+        assert(s_pos <= m_pos)
+        if (s_pos == m_pos):
+            print "m/s binlog matches"
+            break
+        else:
+            print "m/s binlog not matches", m_pos, s_pos
+            if (sleep_cnt >= 10):
+                assert 0, "m/s binlogpos not match"
+            time.sleep(1)
+            sleep_cnt += 1
 
     for i in xrange(10000):
         cli2.send_command("get", str(i))
@@ -33,9 +109,17 @@ def test_repl1(m, s):
 def test_repl():
     m = RedisServer(12345, "m_")
     s = RedisServer(12346, "s_")
-    assert(m.setup(False) == 0)
-    assert(s.setup(False) == 0)
-    test_repl1(m, None)
+    m.setup(False)
+    s.setup(False)
+    test_repl_match1(m, s)
+
+def test_repl1():
+    m = RedisServer(12347, "m_")
+    s = RedisServer(12348, "s_")
+    m.setup(False)
+    s.setup(False)
+    test_repl_match2(m, s)
 
 if __name__ == '__main__':
-    test_repl()
+    #test_repl()
+    test_repl1()
