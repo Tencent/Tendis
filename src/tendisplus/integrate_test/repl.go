7f17b6d0c0cabd01faabdc8ebc6124afbf6447c9
deyukong <deyukong@tencent.com>
2019-02-26 14:03:46 +0800

    add different datastructure options to repl

--- src/tendisplus/integrate_test/repl.go
+++ src/tendisplus/integrate_test/repl.go
@@ -1,24 +1,56 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/mediocregopher/radix.v2/redis"
 	"github.com/ngaut/log"
 	"math/rand"
-	"os"
-	"strconv"
 	"sync"
 	"sync/atomic"
 	"tendisplus/integrate_test/util"
 	"time"
 )
 
+var (
+	mport     = flag.Int("masterport", 0, "master port")
+	sport     = flag.Int("slaveport", 0, "slave port")
+	zsetcount = flag.Int("zsetcount", 1, "zset count")
+	setcount  = flag.Int("setcount", 100, "set count")
+	listcount = flag.Int("listcount", 100, "list count")
+	hashcount = flag.Int("hashcount", 100, "hash count")
+	kvcount   = flag.Int("kvcount", 100000, "kv count")
+	threadnum = flag.Int("threadnum", 4, "thd count")
+	loadsecs  = flag.Int("loadsecs", 20, "seconds for loading data")
+)
+
+type TendisType int
+
+const (
+	KV TendisType = iota
+	SET
+	ZSET
+	LIST
+	HASH
+)
+
+type Record struct {
+	Pk   string
+	Sk   string
+	Type TendisType
+}
+
+func getRandomType() TendisType {
+	typelist := []TendisType{KV, SET, ZSET}
+	return typelist[rand.Int()%len(typelist)]
+}
+
 func testReplMatch2(m, s *util.RedisServer) {
 	var wg sync.WaitGroup
 	running := int32(1)
-	buf := make(chan int)
-	keys := []int{}
-	for i := 0; i < 10; i++ {
+	buf := make(chan *Record)
+	keys := []*Record{}
+	for i := 0; i < *threadnum; i++ {
 		wg.Add(1)
 		go func(idx int) {
 			defer wg.Done()
@@ -26,16 +58,40 @@ func testReplMatch2(m, s *util.RedisServer) {
 			if err != nil {
 				log.Fatalf("dial master failed:%v", err)
 			}
-			cnt := idx * 1000000000
 			for atomic.LoadInt32(&running) == 1 {
-				cnt += 1
-				r, err := cli.Cmd("set", fmt.Sprintf("%d", cnt), fmt.Sprintf("%d", cnt)).Str()
-				if err != nil {
-					log.Fatalf("do set failed:%v", err)
-				} else if r != "OK" {
-					log.Fatalf("do set error:%s", r)
+			    tp := getRandomType()
+				if tp == KV {
+					suffix := rand.Int() % (*kvcount)
+					pk := fmt.Sprintf("kv%d", suffix)
+					r, err := cli.Cmd("set", pk, fmt.Sprintf("%d", suffix)).Str()
+					if err != nil {
+						log.Fatalf("do set failed:%v", err)
+					} else if r != "OK" {
+						log.Fatalf("do set error:%s", r)
+					}
+					buf <- &Record{Pk: pk, Sk: "", Type: tp}
+				} else if tp == SET {
+					suffix := rand.Int() % (*setcount)
+					pk := fmt.Sprintf("set%d", suffix)
+					sk := fmt.Sprintf("sk%d", rand.Int()%10000)
+					_, err := cli.Cmd("sadd", pk, sk).Int()
+					if err != nil {
+						log.Fatalf("do sadd failed:%v", err)
+					}
+					buf <- &Record{Pk: pk, Sk: sk, Type: tp}
+				} else if tp == ZSET {
+					suffix := rand.Int() % (*zsetcount)
+					pk := fmt.Sprintf("zset%d", suffix)
+					sk := fmt.Sprintf("sk%d", rand.Int()%20000000)
+					_, err := cli.Cmd("zrem", pk, sk).Int()
+                    if err != nil {
+						log.Fatalf("do zrem %s %s failed:%v", pk, sk, err)
+					}
+					_, err = cli.Cmd("zadd", pk, rand.Int()%10000000, sk).Int()
+					if err != nil {
+						log.Fatalf("do zadd %s %s failed:%v,%d", pk, sk, err)
+					}
 				}
-				buf <- cnt
 			}
 		}(i)
 	}
@@ -44,18 +100,18 @@ func testReplMatch2(m, s *util.RedisServer) {
 	go func() {
 		wg1.Add(1)
 		defer wg1.Done()
-		for i := range buf {
-			keys = append(keys, i)
+		for o := range buf {
+			keys = append(keys, o)
 		}
 	}()
 
-	time.Sleep(20 * time.Second)
+    time.Sleep(time.Duration(*loadsecs)*time.Second)
 	atomic.StoreInt32(&running, 0)
 	log.Infof("close all producers")
 	wg.Wait()
 	log.Infof("all producers closed")
 	close(buf)
-	log.Infof("quque closed")
+	log.Infof("queue closed")
 	wg1.Wait()
 	log.Infof("consumer closed")
 
@@ -85,13 +141,45 @@ func testReplMatch2(m, s *util.RedisServer) {
 		}
 	}
 	for _, o := range keys {
-		r, err := cli2.Cmd("get", fmt.Sprintf("%d", o)).Str()
-		if err != nil {
-			log.Fatalf("do get on slave failed:%v", err)
-		} else if r != fmt.Sprintf("%d", o) {
-			log.Fatalf("do get on slave bad return:%s", r)
+		if o.Type == KV {
+			r, err := cli2.Cmd("get", o.Pk).Str()
+			if err != nil {
+				log.Fatalf("do get on slave failed:%v", err)
+			}
+			r1, err := cli1.Cmd("get", o.Pk).Str()
+			if err != nil {
+				log.Fatalf("do get on master failed:%v", err)
+			}
+			if r != r1 {
+				log.Fatalf("kv:%s not match", o.Pk)
+			}
+		} else if o.Type == SET {
+			r, err := cli2.Cmd("sismember", o.Pk, o.Sk).Int()
+			if err != nil {
+				log.Fatalf("do sismember on slave failed:%s,%s,%v", o.Pk, o.Sk, err)
+			}
+			r1, err := cli1.Cmd("sismember", o.Pk, o.Sk).Int()
+			if err != nil {
+				log.Fatalf("do sismember on master failed:%v", err)
+			}
+			if r != r1 {
+				log.Fatalf("sismember:%s,%s not match", o, o.Pk, o.Sk)
+			}
+		} else if o.Type == ZSET {
+			r, err := cli2.Cmd("zscore", o.Pk, o.Sk).Str()
+			if err != nil {
+				log.Fatalf("do zscore on slave failed:%v", err)
+			}
+			r1, err := cli1.Cmd("zscore", o.Pk, o.Sk).Str()
+			if err != nil {
+				log.Fatalf("do zscore on master failed:%v", err)
+			}
+			if r != r1 {
+				log.Fatalf("zscore:%s,%s not match", o.Pk, o.Sk)
+			}
 		}
 	}
+	log.Infof("compare complete")
 }
 
 func testReplMatch1(m, s *util.RedisServer) {
@@ -157,13 +245,7 @@ func testRepl(m_port int, s_port int) {
 }
 
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Println("usage: repl m_port s_port")
-		return
-	}
-	m_port, _ := strconv.Atoi(os.Args[1])
-	s_port, _ := strconv.Atoi(os.Args[2])
-
+	flag.Parse()
 	rand.Seed(time.Now().UTC().UnixNano())
-	testRepl(m_port, s_port)
+	testRepl(*mport, *sport)
 }
