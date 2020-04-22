520965836b33dcb82e7f9649b872d2fb93fd0863
takenliu <takenliu@tencent.com>
2020-04-22 17:46:53 +0800

    slowlog support slowlogMaxLen

--- src/tendisplus/commands/command_test.cpp
+++ src/tendisplus/commands/command_test.cpp
@@ -839,15 +839,15 @@ TEST(Command, slowlog) {
     }
     
     fgets(line, sizeof(line)-1, fp);
-    EXPECT_STRCASEEQ(line, "config set slowlog-log-slower-than 0 \n");
+    EXPECT_STRCASEEQ(line, "[] config set slowlog-log-slower-than 0 \n");
     fgets(line, sizeof(line)-1, fp);
-    EXPECT_STRCASEEQ(line, "sadd ss a \n");
+    EXPECT_STRCASEEQ(line, "[] sadd ss a \n");
     fgets(line, sizeof(line)-1, fp);
-    EXPECT_STRCASEEQ(line, "set ss b \n");
+    EXPECT_STRCASEEQ(line, "[] set ss b \n");
     fgets(line, sizeof(line)-1, fp);
-    EXPECT_STRCASEEQ(line, "set ss1 b \n");
+    EXPECT_STRCASEEQ(line, "[] set ss1 b \n");
     fgets(line, sizeof(line)-1, fp);
-    EXPECT_STRCASEEQ(line, "config get slowlog-log-slower-than \n");
+    EXPECT_STRCASEEQ(line, "[] config get slowlog-log-slower-than \n");
     pclose(fp);
 }
 #endif // !
@@ -879,45 +879,31 @@ void testRenameCommand(std::shared_ptr<ServerEntry> svr) {
 }
 
 void testTendisadminSleep(std::shared_ptr<ServerEntry> svr) {
-    asio::io_context ioContext, ioContext2;
-    asio::ip::tcp::socket socket(ioContext), socket2(ioContext2);
-    NetSession sess(svr, std::move(socket), 1, false, nullptr, nullptr);
-    NetSession sess2(svr, std::move(socket2), 1, false, nullptr, nullptr);
+  asio::io_context ioContext, ioContext2;
+  asio::ip::tcp::socket socket(ioContext), socket2(ioContext2);
+  NetSession sess(svr, std::move(socket), 1, false, nullptr, nullptr);
+  NetSession sess2(svr, std::move(socket2), 1, false, nullptr, nullptr);
 
-    int i = 4;
-    std::thread thd1([&sess2, &i]() {
-        uint32_t now = msSinceEpoch();
-        sess2.setArgs({ "tendisadmin", "sleep", std::to_string(i) });
-        auto expect = Command::runSessionCmd(&sess2);
-        auto val = expect.value();
-        EXPECT_TRUE(expect.ok());
-        uint32_t end = msSinceEpoch();
-        EXPECT_TRUE(end - now > (unsigned)(i - 1) * 1000);
-        });
-
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-
-    std::thread thd2([&svr, &i]() {
-        uint32_t now = msSinceEpoch();
-        runCommand(svr, {"ping"});
-        runCommand(svr, {"info"});
-        runCommand(svr, {"info", "replication"});
-        runCommand(svr, {"info", "all"});
-        uint32_t end = msSinceEpoch();
-
-        EXPECT_TRUE(end - now < 500);
-        LOG(INFO) << "info used " << end - now << "ms when running tendisadmin sleep ";
-        });
-
-    sess.setArgs({ "set", "a", "b" });
+  int i = 4;
+  std::thread thd1([&sess2, &i]() {
     uint32_t now = msSinceEpoch();
-    auto expect = Command::runSessionCmd(&sess);
-
+    sess2.setArgs({ "tendisadmin", "sleep", std::to_string(i) });
+    auto expect = Command::runSessionCmd(&sess2);
+    auto val = expect.value();
     EXPECT_TRUE(expect.ok());
     uint32_t end = msSinceEpoch();
-    EXPECT_TRUE(end - now > (unsigned)(i - 2) * 1000);
-    thd1.join();
-    thd2.join();
+    EXPECT_TRUE(end - now > (unsigned) (i - 1) * 1000);
+  });
+
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  sess.setArgs({ "set", "a", "b" });
+  uint32_t now = msSinceEpoch();
+  auto expect = Command::runSessionCmd(&sess);
+
+  EXPECT_TRUE(expect.ok());
+  uint32_t end = msSinceEpoch();
+  EXPECT_TRUE(end - now > (unsigned) (i - 2) * 1000);
+  thd1.join();
 }
 
 void testCommandCommand(std::shared_ptr<ServerEntry> svr) {
@@ -1328,7 +1314,6 @@ TEST(Command, info) {
       {"info", "persistence" },
       {"info", "stats" },
       {"info", "replication" },
-      {"info", "binloginfo" },
       {"info", "cpu" },
       {"info", "commandstats" },
       {"info", "cluster" },
@@ -1339,7 +1324,6 @@ TEST(Command, info) {
       {"info", "levelstats" },
       {"info", "rocksdbstats" },
       {"info", "rocksdbperfstats" },
-      {"info", "rocksdbbgerror" },
       {"info", "invalid" },                 // it's ok
       {"rocksproperty", "rocksdb.base-level", "0" },
       {"rocksproperty", "all", "0" },
@@ -1358,8 +1342,6 @@ TEST(Command, info) {
   {{"config", "resetstat", "stats"}, Command::fmtOK()},
   {{"config", "resetstat", "rocksdbstats"}, Command::fmtOK()},
   {{"config", "resetstat", "invalid"},  Command::fmtOK()},              // it's ok
-  {{"tendisadmin", "sleep", "1"},  Command::fmtOK()}, 
-  {{"tendisadmin", "recovery"},  Command::fmtOK()}, 
   };
 
   std::vector<std::vector<std::string>> wrongArr = {
@@ -1371,10 +1353,6 @@ TEST(Command, info) {
     {"config", "set", "session", "perf_level", "invalid" },
     {"config", "set", "session", "invalid", "invalid" },
     {"config", "set", "session", "perf_level"},
-    {"tendisadmin", "sleep"},
-    {"tendisadmin", "sleep", "1", "2"},
-    {"tendisadmin", "recovery", "1"},
-    {"tendisadmin", "invalid"},
   };
 
   testCommandArray(server, correctArr, false);
