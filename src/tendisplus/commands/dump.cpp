5d53f225cf97441b568ce8e1a81e2649396102c5
eliotwang <eliotwang@tencent.com>
2020-10-28 17:23:01 +0800

    fix typo

--- src/tendisplus/commands/dump.cpp
+++ src/tendisplus/commands/dump.cpp
@@ -253,7 +253,7 @@ class DumpXCommand : public Command {
 
     Command::fmtBulk(ss, dbid);
     Command::fmtBulk(ss, key);
-    if (ttl != std::numeric_limits<int64_t>::max()) {
+    if (ttl != std::numeric_limits<uint64_t>::max()) {
       Command::fmtBulk(ss, std::to_string(ttl));
     }
     Command::fmtBulk(ss, std::string(begin, end));
@@ -1722,7 +1722,9 @@ class IncrMetaCommand : public Command {
           IncrMeta im(key.getPrimaryKey(),
                       ReplOp::REPL_OP_DEL,
                       type,
-                      entry.getTimestamp(), 0, 0);
+                      entry.getTimestamp(),
+                      0,
+                      0);
           return im;
         }
         break;
@@ -1808,7 +1810,7 @@ class IncrMetaCommand : public Command {
         Serializer::saveString(&buf, &pos, key._key);
         easyCopy(&buf, &pos, key._version);
         easyCopy(&buf, &pos, key._ttl);
-        easyCopy(&buf, &pos,  key._type);
+        easyCopy(&buf, &pos, key._type);
         easyCopy(&buf, &pos, key._op);
 #ifdef TENDIS_DEBUG
         LOG(INFO) << "Add primary key:"
