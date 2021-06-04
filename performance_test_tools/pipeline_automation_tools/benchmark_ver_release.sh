4871956ef52b40bf8132c7d5685d474917128e5f
raffertyyu <raffertyyu@tencent.com>
2021-06-04 16:45:04 +0800

    improve dataSize and args parse

--- performance_test_tools/pipeline_automation_tools/benchmark_ver_release.sh
+++ performance_test_tools/pipeline_automation_tools/benchmark_ver_release.sh
@@ -20,24 +20,25 @@ startTask() {
     resultpath="result/tmp-$2"
     logInfo "$tast tmp result path: $resultpath"
     mkdir -p ${resultpath}
+    dataSize=$3
     for((i=0;i<$benchnum;i++))
     do
         ip=${IParr[$i]}
-        cmdPref="$benmark_binary -t ${threadnum} -c ${clientnum} -s $ip -p $port --distinct-client-seed -a $pw --test-time=${testTime}"
-        if [ "$task" = "set" ]; then
-            $cmdPref --command='set __key__ __data__' --key-prefix='kv_' --key-minimum=1 --key-maximum=500000000 --random-data --data-size=128 -o ${resultpath}/${i} &
-        elif [ "$task" = "get" ]; then
-            $cmdPref --command='get __key__' --key-prefix='kv_' --key-minimum=1 --key-maximum=500000000 -o ${resultpath}/${i} &
+        cmdPref="$benmark_binary -t ${threadnum} -c ${clientnum} -s $ip -p $port --distinct-client-seed -a $pw --test-time=${testTime} -o ${resultpath}/${i}"
+        if [ $task = "set" ]; then
+            $cmdPref --command='set __key__ __data__' --key-prefix='kv_' --key-minimum=1 --key-maximum=500000000 --random-data --data-size=${dataSize} &
+        elif [ $task = "get" ]; then
+            $cmdPref --command='get __key__' --key-prefix='kv_' --key-minimum=1 --key-maximum=500000000 &
         elif [ $task = "incr" ]; then
-            $cmdPref --command='incr __key__' --key-prefix='int_' --key-minimum=1 --key-maximum=1000000 -o ${resultpath}/${i} &
+            $cmdPref --command='incr __key__' --key-prefix='int_' --key-minimum=1 --key-maximum=1000000 &
         elif [ $task = "lpush" ]; then
-            $cmdPref --command='lpush __key__ __data__' --key-prefix='list_' --key-minimum=1 --key-maximum=1000000 --random-data --data-size=128 -o ${resultpath}/${i} &
+            $cmdPref --command='lpush __key__ __data__' --key-prefix='list_' --key-minimum=1 --key-maximum=1000000 --random-data --data-size=${dataSize} &
         elif [ $task = "sadd" ]; then
-            $cmdPref --command='sadd __key__ __data__' --key-prefix='set_' --key-minimum=1 --key-maximum=1000000 --random-data --data-size=128 -o ${resultpath}/${i} &
+            $cmdPref --command='sadd __key__ __data__' --key-prefix='set_' --key-minimum=1 --key-maximum=1000000 --random-data --data-size=${dataSize} &
         elif [ $task = "zadd" ]; then
-            $cmdPref --command='zadd __key__ __key__ __data__' --key-prefix='' --key-minimum=1 --key-maximum=1000000 --random-data --data-size=128 -o ${resultpath}/${i} &
+            $cmdPref --command='zadd __key__ __key__ __data__' --key-prefix='' --key-minimum=1 --key-maximum=1000000 --random-data --data-size=${dataSize} &
         elif [ $task = "hset" ]; then
-            $cmdPref --command='hset __key__ __data__ __data__' --key-prefix='hash_' --key-minimum=1 --key-maximum=1000000 --random-data --data-size=128 -o ${resultpath}/${i} &
+            $cmdPref --command='hset __key__ __data__ __data__' --key-prefix='hash_' --key-minimum=1 --key-maximum=1000000 --random-data --data-size=${dataSize} &
         fi
     done
 }
@@ -55,22 +56,10 @@ waitFinish() {
     logInfo "$tendisVersion task finished: $1"
 }
 
-if [[ $# != 5 ]]
-then
-    echo "usage: "
-    echo "    nohup bash benchmark_ver_release.sh tendisVersion shortTimeTestList testTime longTimeTestList testTime &"
-    echo "example: "
-    echo "    nohup bash benchmark_ver_release.sh 2-3-4 set,get 30 set 1800 &"
-    exit 1
-fi
-
 logInfo "========start========"
 tendisVersion=$1
+shift
 logInfo "start tendisVersion: $tendisVersion benchmark"
-shortTimeTestList=$2
-shortTestTime=$3
-longTimeTestList=$4
-longTestTime=$5
 
 if [ ! -f ./k8s.sh ]
 then
@@ -84,8 +73,7 @@ benmark_binary=./memtier_benchmark
 clientnum=50
 threadnum=20
 benchnum=3
-interTime=120
-defaultTestTime=600
+interTime=300
 
 decreaseLimit_set=10
 decreaseLimit_get=10
@@ -116,115 +104,123 @@ outputReport() {
 }
 
 runTest() {
-    mailfile=Report.txt
-    rm ${mailfile}
-    #1e8 need about 8 minutes
-    initTimeStamp=$(date +%s)
-    for itest in $(echo $1 | tr ',' '\n')
+    for ivalue in $(echo $2 | tr ',' '\n')
     do
-        testTime=${defaultTestTime}
-        curTestTime=$2
-        if [[ "$curTestTime" == "0" ]]
-        then
-            return 0
-        fi
-        startTimestamp=$(date +%s)
-        while [[ "$curTestTime" != "0" ]]
+        mailfile=Report.txt
+        rm ${mailfile}
+        #1e8 need about 8 minutes
+        initTimeStamp=$(date +%s)
+        for itest in $(echo $1 | tr ',' '\n')
         do
-            if [[ "$curTestTime" -gt "$testTime" ]]
-            then
-                let curTestTime=curTestTime-testTime
-            else
-                testTime=${curTestTime}
-                curTestTime=0
-            fi
-            startTask ${itest} ${startTimestamp}
+            testTime=${3}
+            startTimestamp=$(date +%s)
+            startTask ${itest} ${startTimestamp} ${ivalue}
             waitFinish ${itest}
-        done
-        testTime=${defaultTestTime}
-        endTimestamp=$(date +%s)
-        resultpath="result/tmp-$startTimestamp"
-        AVG=0.0
-        P50=0.0
-        P99=0.0
-        P100=0.0
+            endTimestamp=$(date +%s)
+            resultpath="result/tmp-$startTimestamp"
+            AVG=0.1
+            P50=0.1
+            P99=0.1
+            P100=0.1
 
-        for f in $(ls ${resultpath})
-        do
-            tmpAVG=$(cat ${resultpath}/$f | grep -i ${itest}s | tail -n 1 | awk '{print $3}')
-            if [[ ! -z $tmpAVG ]]; then
-                if [ 1 -eq "$(echo "${tmpAVG} > ${AVG}" | bc)" ]; then
-                    AVG=${tmpAVG}
+            for f in $(ls ${resultpath})
+            do
+                tmpAVG=$(cat ${resultpath}/$f | grep -i ${itest}s | tail -n 1 | awk '{print $3}')
+                if [[ ! -z $tmpAVG ]]; then
+                    if [ 1 -eq "$(echo "${tmpAVG} > ${AVG}" | bc)" ]; then
+                        AVG=${tmpAVG}
+                    fi
                 fi
-            fi
-            tmpP50=$(cat ${resultpath}/$f | grep -i ${itest}s | tail -n 1 | awk '{print $4}')
-            if [[ ! -z $tmpP50 ]]; then
-                if [ 1 -eq "$(echo "${tmpP50} > ${P50}" | bc)" ]; then
-                    P50=${tmpP50}
+                tmpP50=$(cat ${resultpath}/$f | grep -i ${itest}s | tail -n 1 | awk '{print $4}')
+                if [[ ! -z $tmpP50 ]]; then
+                    if [ 1 -eq "$(echo "${tmpP50} > ${P50}" | bc)" ]; then
+                        P50=${tmpP50}
+                    fi
                 fi
-            fi
-            tmpP99=$(cat ${resultpath}/$f | grep -i ${itest}s | tail -n 1 | awk '{print $5}')
-            if [[ ! -z $tmpP99 ]]; then
-                if [ 1 -eq "$(echo "${tmpP99} > ${P99}" | bc)" ]; then
-                    P99=${tmpP99}
+                tmpP99=$(cat ${resultpath}/$f | grep -i ${itest}s | tail -n 1 | awk '{print $5}')
+                if [[ ! -z $tmpP99 ]]; then
+                    if [ 1 -eq "$(echo "${tmpP99} > ${P99}" | bc)" ]; then
+                        P99=${tmpP99}
+                    fi
                 fi
-            fi
-            tmpP100=$(cat ${resultpath}/$f | grep -i ${itest}s | tail -n 1 | awk '{print $6}')
-            if [[ ! -z $tmpP100 ]]; then
-                if [ 1 -eq "$(echo "${tmpP100} > ${P100}" | bc)" ]; then
-                    P100=${tmpP100}
+                tmpP100=$(cat ${resultpath}/$f | grep -i ${itest}s | tail -n 1 | awk '{print $6}')
+                if [[ ! -z $tmpP100 ]]; then
+                    if [ 1 -eq "$(echo "${tmpP100} > ${P100}" | bc)" ]; then
+                        P100=${tmpP100}
+                    fi
                 fi
+            done
+            let duration=${endTimestamp}-${startTimestamp}
+            qps=$(curl -g "http://${prometheusURL}/api/v1/query?query=sum(increase(redis_command_call_duration_seconds_count{gcs_app=\"${appname}\",gcs_cluster=\"${clusterprefix}-${tendisVersion}\",gcs_dbrole=\"master\",cmd=\"${itest}\"}[${duration}s]))by(cmd)&time=${endTimestamp}" 2>/dev/null | tr "\"" " " | awk '{print $(NF-1)}')
+            qps=$(echo $qps / $duration | bc -l)
+            decreaseLimit=''
+            if [[ "$itest" == "set" ]]; then
+                decreaseLimit=${decreaseLimit_set}
+                outputReport "测试命令(${benchnum}个): $benmark_binary -t ${threadnum} -c ${clientnum} --distinct-client-seed --test-time=${testTime} --command='set __key__ __data__' --key-prefix='kv_' --key-minimum=1 --key-maximum=500000000 --random-data --data-size=${ivalue}"
+            elif [[ "$itest" == "get" ]]; then
+                decreaseLimit=${decreaseLimit_get}
+                outputReport "测试命令(${benchnum}个): $benmark_binary -t ${threadnum} -c ${clientnum} --distinct-client-seed --test-time=${testTime} --command='get __key__' --key-prefix='kv_' --key-minimum=1 --key-maximum=500000000"
+            elif [[ "$itest" == "incr" ]]; then
+                decreaseLimit=${decreaseLimit_incr}
+                outputReport "测试命令(${benchnum}个): $benmark_binary -t ${threadnum} -c ${clientnum} --distinct-client-seed --test-time=${testTime} --command='incr __key__' --key-prefix='int_' --key-minimum=1 --key-maximum=1000000"
+            elif [[ "$itest" == "lpush" ]]; then
+                decreaseLimit=${decreaseLimit_lpush}
+                outputReport "测试命令(${benchnum}个): $benmark_binary -t ${threadnum} -c ${clientnum} --distinct-client-seed --test-time=${testTime} --command='lpush __key__ __data__' --key-prefix='list_' --key-minimum=1 --key-maximum=1000000 --random-data --data-size=${ivalue}"
+            elif [[ "$itest" == "sadd" ]]; then
+                decreaseLimit=${decreaseLimit_sadd}
+                outputReport "测试命令(${benchnum}个): $benmark_binary -t ${threadnum} -c ${clientnum} --distinct-client-seed --test-time=${testTime} --command='sadd __key__ __data__' --key-prefix='set_' --key-minimum=1 --key-maximum=1000000 --random-data --data-size=${ivalue}"
+            elif [[ "$itest" == "zadd" ]]; then
+                decreaseLimit=${decreaseLimit_zadd}
+                outputReport "测试命令(${benchnum}个): $benmark_binary -t ${threadnum} -c ${clientnum} --distinct-client-seed --test-time=${testTime} --command='zadd __key__ __key__ __data__' --key-prefix='' --key-minimum=1 --key-maximum=1000000 --random-data --data-size=${ivalue}"
+            elif [[ "$itest" == "hset" ]]; then
+                decreaseLimit=${decreaseLimit_hset}
+                outputReport "测试命令(${benchnum}个): $benmark_binary -t ${threadnum} -c ${clientnum} --distinct-client-seed --test-time=${testTime} --command='hset __key__ __data__ __data__' --key-prefix='hash_' --key-minimum=1 --key-maximum=1000000 --random-data --data-size=${ivalue}"
             fi
-        done
-        let duration=${endTimestamp}-${startTimestamp}
-        qps=$(curl -g "http://${prometheusURL}/api/v1/query?query=sum(increase(redis_command_call_duration_seconds_count{gcs_app=\"${appname}\",gcs_cluster=\"${clusterprefix}-${tendisVersion}\",gcs_dbrole=\"master\",cmd=\"${itest}\"}[${duration}s]))by(cmd)&time=${endTimestamp}" 2>/dev/null | tr "\"" " " | awk '{print $(NF-1)}')
-        qps=$(echo $qps / $duration | bc -l)
-        decreaseLimit=''
-        if [[ "$itest" == "set" ]]; then
-            decreaseLimit=${decreaseLimit_set}
-            outputReport "测试命令(${benchnum}个): $benmark_binary -t ${threadnum} -c ${clientnum} --distinct-client-seed --test-time=${testTime} --command='set __key__ __data__' --key-prefix='kv_' --key-minimum=1 --key-maximum=500000000 --random-data --data-size=128"
-        elif [[ "$itest" == "get" ]]; then
-            decreaseLimit=${decreaseLimit_get}
-            outputReport "测试命令(${benchnum}个): $benmark_binary -t ${threadnum} -c ${clientnum} --distinct-client-seed --test-time=${testTime} --command='get __key__' --key-prefix='kv_' --key-minimum=1 --key-maximum=500000000"
-        elif [[ "$itest" == "incr" ]]; then
-            decreaseLimit=${decreaseLimit_incr}
-            outputReport "测试命令(${benchnum}个): $benmark_binary -t ${threadnum} -c ${clientnum} --distinct-client-seed --test-time=${testTime} --command='incr __key__' --key-prefix='int_' --key-minimum=1 --key-maximum=1000000"
-        elif [[ "$itest" == "lpush" ]]; then
-            decreaseLimit=${decreaseLimit_lpush}
-            outputReport "测试命令(${benchnum}个): $benmark_binary -t ${threadnum} -c ${clientnum} --distinct-client-seed --test-time=${testTime} --command='lpush __key__ __data__' --key-prefix='list_' --key-minimum=1 --key-maximum=1000000 --random-data --data-size=128"
-        elif [[ "$itest" == "sadd" ]]; then
-            decreaseLimit=${decreaseLimit_sadd}
-            outputReport "测试命令(${benchnum}个): $benmark_binary -t ${threadnum} -c ${clientnum} --distinct-client-seed --test-time=${testTime} --command='sadd __key__ __data__' --key-prefix='set_' --key-minimum=1 --key-maximum=1000000 --random-data --data-size=128"
-        elif [[ "$itest" == "zadd" ]]; then
-            decreaseLimit=${decreaseLimit_zadd}
-            outputReport "测试命令(${benchnum}个): $benmark_binary -t ${threadnum} -c ${clientnum} --distinct-client-seed --test-time=${testTime} --command='zadd __key__ __key__ __data__' --key-prefix='' --key-minimum=1 --key-maximum=1000000 --random-data --data-size=128"
-        elif [[ "$itest" == "hset" ]]; then
-            decreaseLimit=${decreaseLimit_hset}
-            outputReport "测试命令(${benchnum}个): $benmark_binary -t ${threadnum} -c ${clientnum} --distinct-client-seed --test-time=${testTime} --command='hset __key__ __data__ __data__' --key-prefix='hash_' --key-minimum=1 --key-maximum=1000000 --random-data --data-size=128"
-        fi
-        outputReport "${itest}测试曲线：<a href=\"${grafanaURL}-${tendisVersion}&from=${startTimestamp}000&to=${endTimestamp}000\">${grafanaURL}-${tendisVersion}&from=${startTimestamp}000&to=${endTimestamp}000</a>"
-        python writeTag.py ${itest} ${tendisVersion} $(date +%Y%m%d) ${qps} ${P50} ${P99} ${P100} ${AVG} ${mailfile} ${decreaseLimit} ${decreaseLimit_p50} ${decreaseLimit_p99} ${decreaseLimit_p100} ${decreaseLimit_pavg}
+            outputReport "${itest}测试曲线：<a href=\"${grafanaURL}-${tendisVersion}&from=${startTimestamp}000&to=${endTimestamp}000\">${grafanaURL}-${tendisVersion}&from=${startTimestamp}000&to=${endTimestamp}000</a>"
+            python writeTag.py ${itest} ${tendisVersion} $(date +%Y%m%d) ${qps} ${P50} ${P99} ${P100} ${AVG} ${mailfile} ${decreaseLimit} ${decreaseLimit_p50} ${decreaseLimit_p99} ${decreaseLimit_p100} ${decreaseLimit_pavg}
 
-        if [[ "$itest" != "set" ]]
-        then
-            ./flushdb.exp ${K8sClusterNode} ${user} ${K8sNodePW} ${K8sNodeHN} ${K8sNodePort} ${K8sNodeGrepString} ${appname}
-        fi
-        sleep $interTime
+            if [[ "$itest" != "set" ]]
+            then
+                ./flushdb.exp ${K8sClusterNode} ${user} ${K8sNodePW} ${K8sNodeHN} ${K8sNodePort} ${K8sNodeGrepString} ${appname}
+            fi
+            sleep $interTime
+        done
+        finalTimeStamp=$(date +%s)
+
+        grafanaStartTimestamp=${initTimeStamp}000
+        grafanaEndTimestamp=${finalTimeStamp}000
+        logInfo "${tendisVersion} grafanaStartTimestamp:$grafanaStartTimestamp grafanaEndTimestamp:$grafanaEndTimestamp"
+        mv ${mailfile} ${mailfile}.bak
+        outputReport "全过程监控<a href=\"${grafanaURL}-${tendisVersion}&from=${grafanaStartTimestamp}&to=${grafanaEndTimestamp}\">${grafanaURL}-${tendisVersion}&from=${grafanaStartTimestamp}&to=${grafanaEndTimestamp}</a>"
+        cat ${mailfile}.bak >> ${mailfile}
+        rm ${mailfile}.bak
+        /data/Anaconda2/bin/python sendmail.py ${tendisVersion} ${passid} ${token} ${mailURL} ${mailSender} ${sendmailgroup}
     done
-    finalTimeStamp=$(date +%s)
-
-    grafanaStartTimestamp=${initTimeStamp}000
-    grafanaEndTimestamp=${finalTimeStamp}000
-    logInfo "${tendisVersion} grafanaStartTimestamp:$grafanaStartTimestamp grafanaEndTimestamp:$grafanaEndTimestamp"
-    mv ${mailfile} ${mailfile}.bak
-    outputReport "全过程监控<a href=\"${grafanaURL}-${tendisVersion}&from=${grafanaStartTimestamp}&to=${grafanaEndTimestamp}\">${grafanaURL}-${tendisVersion}&from=${grafanaStartTimestamp}&to=${grafanaEndTimestamp}</a>"
-    cat ${mailfile}.bak >> ${mailfile}
-    rm ${mailfile}.bak
-    /data/Anaconda2/bin/python sendmail.py ${tendisVersion} ${passid} ${token} ${mailURL} ${mailSender} ${sendmailgroup}
 }
 
-runTest $shortTimeTestList $shortTestTime
-runTest $longTimeTestList $longTestTime
+while [[ $# -gt 0 ]]
+do
+    if [[ $# -lt 3 ]]; then
+        echo "error arg $@"
+        echo "usage:"
+        echo "    nohup bash benchmark_ver_release.sh tendisVersion [testList, testValList, testTime]..."
+        echo "example:"
+        echo "    nohup bash benchmark_ver_release.sh 2-3-4 set,get,sadd 64,1024 1800 zadd,hset 64 1800 &"
+        echo ""
+        echo "  testList can be one or more commands, with comma connected"
+        echo "  testValList can be one or more commands, with comma connected"
+        echo "  testTime should be explicit one integer."
+        exit 1
+    fi
+    testList=$1
+    testValueList=$2
+    testTime=$3
+    shift
+    shift
+    shift
+    echo "runTest" $testList $testValueList $testTime
+    runTest $testList $testValueList $testTime
+done
 
 logInfo "end tendisVersion: $tendisVersion benchmark"
 logInfo "========end========"
