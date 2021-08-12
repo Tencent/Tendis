7005e9e5bba2b6a09a8eb3b9a2365cbeaefb7d56
raffertyyu <raffertyyu@tencent.com>
2021-08-12 20:35:57 +0800

    support various baselineVersion

--- performance_test_tools/pipeline_automation_tools/sendmail.py
+++ performance_test_tools/pipeline_automation_tools/sendmail.py
@@ -4,19 +4,19 @@
 import sys
 from pytof4 import Tof4Mail
 
-PAASID = sys.argv[2]
-TOKEN = sys.argv[3]
-URL = sys.argv[4]
+PAASID = sys.argv[3]
+TOKEN = sys.argv[4]
+URL = sys.argv[5]
 msg = Tof4Mail(PAASID, TOKEN, URL)
 msg.Title = sys.argv[1] + "性能测试报告"
-with open("Report.txt",'r') as f:
+with open(sys.argv[2],'r') as f:
     s=''
     for l in f.readlines():
         s+='<p>'+l.replace('\n', '')+'</p>'
     msg.Content=s
-msg.From = sys.argv[5]
-if len(sys.argv) > 6:
-    msg.To = sys.argv[6]
+msg.From = sys.argv[6]
+if len(sys.argv) > 7:
+    msg.To = sys.argv[7]
 else:
     msg.To = ''
 msg.CC = ''
