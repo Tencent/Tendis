e446c251ceabcbec52f36f02b32c2a9a5cc11c9f
zakzheng <zakzheng@tencent.com>
2022-02-19 22:01:09 +0800

    issue 250 update tendisplus version

--- src/tendisplus/commands/version.h
+++ src/tendisplus/commands/version.h
@@ -6,7 +6,7 @@
 #define SRC_TENDISPLUS_COMMANDS_VERSION_H_
 
 #include <string>
-#define TENDISPLUS_VERSION_PRE "2.3.1-rocksdb-v"
+#define TENDISPLUS_VERSION_PRE "2.4.4-rocksdb-v"
 
 std::string getTendisPlusVersion();
 
