d9f3c82d7ae22e0ba00795c8736f3a2294ea714d
takenliu <takenliu@tencent.com>
2024-10-10 09:57:13 +0800

    create cluster client need check auth.

--- src/tendisplus/cluster/cluster_manager.h
+++ src/tendisplus/cluster/cluster_manager.h
@@ -945,6 +945,7 @@ class ClusterManager {
     const std::shared_ptr<ClusterNode>& node);
   Expected<std::shared_ptr<BlockingTcpClient>> clusterCreateClient(
     const std::shared_ptr<ClusterNode>& node);
+  bool clusterAuth(const std::shared_ptr<ClusterNode>& node);
 
   NetworkAsio* getClusterNetwork() const;
   std::shared_ptr<ClusterState> getClusterState() const;
