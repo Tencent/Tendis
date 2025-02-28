4134c88d9dbdd87edb933aeab61b49921d8ecc4d
raffertyyu <raffertyyu@tencent.com>
2025-02-28 11:39:00 +0800

    escape non-ascii character shown in ldb_tendis key

--- src/tendisplus/tools/ldb_tendis.cpp
+++ src/tendisplus/tools/ldb_tendis.cpp
@@ -256,6 +256,32 @@ void TScanCommand::printLog(const std::string& value) {
   }
 }
 
+std::string escapeNonPrintableAndNonAscii(const std::string& input) {
+  std::ostringstream oss;
+  for (const char c : input) {
+    unsigned char uc = static_cast<unsigned char>(c);
+    if (uc == '\a') {
+      oss << "\\a";
+    } else if (uc == '\b') {
+      oss << "\\b";
+    } else if (uc == '\t') {
+      oss << "\\t";
+    } else if (uc == '\n') {
+      oss << "\\n";
+    } else if (uc == '\r') {
+      oss << "\\r";
+    } else if (uc == '\\') {
+      oss << "\\\\";
+    } else if (uc >= 32 && uc <= 126) {
+      oss << c;
+    } else {
+      oss << "\\x" << std::hex << std::setw(2) << std::setfill('0')
+          << static_cast<int>(uc);
+    }
+  }
+  return oss.str();
+}
+
 void TScanCommand::DoCommand() {
   if (!db_) {
     assert(GetExecuteState().IsFailed());
@@ -376,13 +402,16 @@ void TScanCommand::DoCommand() {
       auto pt = reinterpret_cast<uint64_t*>(const_cast<char*>(pKey.data()));
       pKey = std::to_string(htobe64(*pt));
     }
+
+    auto hexKey = escapeNonPrintableAndNonAscii(pKey);
+
     // type dbid PrimaryKey ttl value(kv) subKeysCount(list/set/zset/hash)
     fprintf(stdout,
             "%c %" PRIu32 " %.*s %" PRIu64 " %" PRIu64 " %" PRIu64 "\n",
             type,
             rk.getDbId(),
-            static_cast<int>(pKey.size()),
-            pKey.data(),
+            static_cast<int>(hexKey.size()),
+            hexKey.data(),
             rv.getTtl(),
             vallen,
             fieldlen);
