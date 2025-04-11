aef03bc7a39d30834dea7835fa11dc3297ea9956
takenliu(Liurui) <takenliu@tencent.com>
2025-04-11 10:50:39 +0800

    2.7.6

--- src/tendisplus/commands/version.h
+++ src/tendisplus/commands/version.h
@@ -6,7 +6,7 @@
 #define SRC_TENDISPLUS_COMMANDS_VERSION_H_
 
 #include <string>
-#define TENDISPLUS_VERSION_PRE "2.7.4-rocksdb-v"
+#define TENDISPLUS_VERSION_PRE "2.7.6-rocksdb-v"
 
 std::string getTendisPlusVersion();
 
