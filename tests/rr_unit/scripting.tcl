871f11bfe787823b84c00fc6bc6ef657417efaa3
KIMDONGYEON00 <dongyeonkim2000@gmail.com>
2025-10-20 15:16:48 +0900

    Add test for lua bit.tohex with negative value
    Added a test for lua bit.tohex function with edge case.

--- tests/rr_unit/scripting.tcl
+++ tests/rr_unit/scripting.tcl
@@ -558,7 +558,6 @@ start_server {tags {"scripting repl"}} {
         #        fail "Expected 1 in x, but value is '[r -1 get x]'"
         #    }
         #}
-
         test {Lua scripts using SELECT are replicated correctly} {
             r eval {
                 redis.call("set","foo1","bar1")
@@ -580,5 +579,10 @@ start_server {tags {"scripting repl"}} {
                 fail "Master-Slave desync after Lua script using SELECT."
             }
         }
+        test {lua bit.tohex bug} {
+            set res [run_script {return bit.tohex(65535, -2147483648)} 0]
+            r ping
+            set res
+        } {0000FFFF}
     }
 }
