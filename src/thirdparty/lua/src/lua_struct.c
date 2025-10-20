b562a8ef33a6cb8f784701ec25079cf91da8e460
KIMDONGYEON00 <dongyeonkim2000@gmail.com>
2025-10-20 16:00:21 +0900

    Fix integral size overflow error message

--- src/thirdparty/lua/src/lua_struct.c
+++ src/thirdparty/lua/src/lua_struct.c
@@ -96,7 +96,7 @@ static int getnum (lua_State *L, const char **fmt, int df) {
     int a = 0;
     do {
       if (a > (INT_MAX / 10) || a * 10 > (INT_MAX - (**fmt - '0')))
-      luaL_error(L, "integral size overflow");
+        luaL_error(L, "integral size overflow");
       a = a*10 + *((*fmt)++) - '0';
     } while (isdigit(**fmt));
     return a;
