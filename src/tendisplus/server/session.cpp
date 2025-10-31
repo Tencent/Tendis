a91c48be623dd5c32bbf1262272bf7031581bbad
earl86 <wang_chao86@126.com>
2025-10-31 10:19:04 +0800

    优化部分 压测时 QPS 降低严重的问题

--- src/tendisplus/server/session.cpp
+++ src/tendisplus/server/session.cpp
@@ -321,7 +321,7 @@ LocalSessionGuard::~LocalSessionGuard() {
       cmds = "null cmd.";
     }
     LOG(WARNING) << "latency too long localsession cmd:" << cmds << " " << s
-                  << " threadid:" << getCurThreadId();
+                 << " threadid:" << getCurThreadId();
   }
 }
 
