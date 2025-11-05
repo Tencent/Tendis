58f4199a23ab4986780b2b68a30b291a3456dd5e
earl86 <wang_chao86@126.com>
2025-11-05 09:53:34 +0800

    优化压测时部分命令慢的问题,提升性能

--- src/tendisplus/server/session.cpp
+++ src/tendisplus/server/session.cpp
@@ -296,23 +296,26 @@ LocalSessionGuard::LocalSessionGuard(ServerEntry* svr, Session* sess)
 
 LocalSessionGuard::~LocalSessionGuard() {
   // don't call svr->endSession(_sess->id());
-  std::stringstream ss;
-  for (uint8_t i = 0; i < LockLatencyType::MAX_LLT; ++i) {
-    auto lockRecord = _sess->getCtx()->generateLockRecordLogIfNeeded(
-      static_cast<LockLatencyType>(i));
-    if (!lockRecord.empty()) {
-      ss << lockRecord << " ";
+  std::string s;
+  if (_sess->getServerEntry()->getParams()->tendisLatencyLimit > 0) {
+    for (uint8_t i = 0; i < LockLatencyType::MAX_LLT; ++i) {
+      auto lockRecord = _sess->getCtx()->generateLockRecordLogIfNeeded(
+        static_cast<LockLatencyType>(i));
+      if (!lockRecord.empty()) {
+        s.append(lockRecord).append(" ");
+      }
     }
   }
-  for (uint8_t i = 0; i < RocksdbLatencyType::MAX_RLT; ++i) {
-    auto rocksdbRecord = _sess->getCtx()->generateRocksdbRecordLogIfNeeded(
-      static_cast<RocksdbLatencyType>(i));
-    if (!rocksdbRecord.empty()) {
-      ss << rocksdbRecord << " ";
+  if (_sess->getServerEntry()->getParams()->rocksdbLatencyLimit > 0) {
+    for (uint8_t i = 0; i < RocksdbLatencyType::MAX_RLT; ++i) {
+      auto rocksdbRecord = _sess->getCtx()->generateRocksdbRecordLogIfNeeded(
+        static_cast<RocksdbLatencyType>(i));
+      if (!rocksdbRecord.empty()) {
+        s.append(rocksdbRecord).append(" ");
+      }
     }
   }
 
-  const std::string& s = ss.str();
   if (!s.empty()) {
     std::string cmds;
     if (_sess && !_sess->getArgs().empty()) {
